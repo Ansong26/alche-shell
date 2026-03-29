f29cb232d6fc465f125c73d06d8852f80a274680
Ansong26 <f.ansong@alustudent.com>
2026-03-28 22:56:11 -0700

    Added the file 0-hello_world

---
+++ io_redirections_and_filters/README.md
@@ -0,0 +1,1 @@
+In this project I will be practicing Shell I/O, redirections and filters
---
+++ io_redirections_and_filters/watchme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter file name" file
+read -p "Enter command" cmd
+
+echo -e "#!/bin/bash\n${cmd}" >${file}
+chmod u+x ${file}
+
+git add .
+git commit -m "Added the file ${file}"
+git push
+~             
